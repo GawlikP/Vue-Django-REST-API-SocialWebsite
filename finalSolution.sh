37544493d781666393edd9d284d654eb4323c3e2
GawlikP <rudepeeter@gmail.com>
2021-02-18 16:49:17 +0100

    FinalSoltiion.sh - dodano skrypt nadpisujacy branche

---
+++ finalSolution.sh
@@ -0,0 +1,14 @@
+git checkout gawlik
+git merge main
+git checkout boli
+git merge main
+git checkout bruno
+git merge main
+git checkout jacek
+git merge main
+git checkout natalia
+git merge main
+git push origin gawlik 
+git push origin boli
+git push origin bruno
+git push origin natalia
