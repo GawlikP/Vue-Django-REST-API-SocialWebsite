d5de2e7e65e01b72c0ef79310b1a816e6e1c37e1
jacekchabielski <jacekchabielski@gmail.com>
2021-02-21 21:17:47 +0100

    testowanie srodowiska, wstawianie dupereli do about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <PostFetchComponent/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+import PostFetchComponent from '@/components/PostFetchComponent.vue'
+
+export default {
+  name: 'About',
+  components: {
+      PostFetchComponent
+  }
+}
+</script>
