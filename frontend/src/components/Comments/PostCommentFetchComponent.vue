7c0b68cf25f00fbb931e1f8993dd5c6f1ad481e4
kamilbolibrzuch <kamil.bolibrzuch@gmail.com>
2021-05-15 20:48:29 +0200

    Co-authored-by: jacek.chabielski@outlook.com <jacek.chabielski@outlook.com>

--- frontend/src/components/Comments/PostCommentFetchComponent.vue
+++ frontend/src/components/Comments/PostCommentFetchComponent.vue
@@ -2,27 +2,46 @@
     <div >
   
         <div class="container" >
-            
+    <a data-toggle="tooltip" title="Komentarze" class="m-2"  role="button" style="color: rgb(59, 89, 152);" @click="collapse1 = !collapse1"  >
+        <i class="fas fa-comments fa-lg"></i>
+    <MDBIcon iconstyle="fab"  size="lg"></MDBIcon>
+  </a> 
 
-    <tbody v-for="comment in comments" :key="comment.id" style="text-align:left">
-        <tr>
-           <strong>{{authors[comment.id].username}}</strong>
-        </tr>
-        <tr>
-        {{comment.content}}
-        </tr>
-        
-        <hr> 
-        
-       
+    <tbody v-for="comment in comments" :key="comment.id"  style="text-align:left;">
+
+    
+        <MDBCollapse id="collapsibleContent1" v-model="collapse1">
+           <table class="borderless" style="text-align:left">
+                <tr style="color:black">
+                    <strong>{{authors[comment.id].username}}</strong>
+                </tr>
+
+                <tr style="color:black">
+                    {{comment.content}}
+                </tr>
+            </table>
+        </MDBCollapse> 
+               
     </tbody>
- 
+    
+        <MDBCollapse id="collapsibleContent1" v-model="collapse1">
+            <div class="form-outline mt-4">
+                <form>
+                    <MDBInput  label="twój komentarz..." white style="background-color:silver" size="lg" type="text"  class="w-50" />
+                    <br>
+                    <MDBBtn color="danger" >Dodaj komentarz</MDBBtn >
+                    
+                </form>
+            </div>     
+        </MDBCollapse> 
+                
     </div>
     </div>
 </template>
 
 <script>
-
+     import { MDBCollapse, MDBBtn, MDBInput, MDBIcon } from "mdb-vue-ui-kit";
+     import { ref } from 'vue';
 //import { MDBTable  } from "mdb-vue-ui-kit";
 export default {
 
@@ -31,6 +50,11 @@ export default {
         post_id: Number,
     },
     components: {
+          MDBCollapse,
+          MDBBtn,
+          MDBInput,
+          MDBIcon
+          
      //   MDBTable
     },
     async created() {
@@ -68,6 +92,13 @@ export default {
             var json = await response.json();
             this.authors[comment.id] = json;
         }
+    },
+     setup() {
+      const collapse1 = ref(false);
+
+      return {
+        collapse1
+      }
     }
 }
 </script>
--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="container" >
-  <center>  <img alt="Vue logo" src="../assets/backendsiebawi.png">
-        <h1> Witaj młody byku, wszedłeś na terytorium testowania backendu <br>
-        Zachowaj spokój, otwórz kustosza i kontynuuj swoją podróż</h1>    
-      <h1 v-if="username">{{username}}</h1>
+   <center>
+      
+        <PostFormComponent/>
         <PostFetchComponent/>
 
-        <PostFormComponent/>
-    </center>
+        
+   </center>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
 import PostFetchComponent from '@/components/Posts/PostFetchComponent.vue'
 import PostFormComponent from '@/components/Posts/PostFormComponent.vue';
-
 export default {
   name: 'Posts',
   components: {
@@ -27,13 +24,11 @@ export default {
     return{
       username : "" 
     }
-
   },
   created(){
     if(window.sessionStorage.getItem("username")){
       this.username = window.sessionStorage.getItem("username");
     }
   }
-
 }
 </script>
